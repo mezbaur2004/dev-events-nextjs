9a7b54406fb65976630bd247bd3978dd56a18fce
mezbaur2004 <mezbaur2004@gmail.com>
2026-01-01 19:46:43 +0600

    each event details

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -3,10 +3,29 @@ import {notFound} from "next/dist/client/components/not-found";
 import Image from "next/image";
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
+const EventDetailItem=({icon,alt,label}:{icon:string,alt:string,label:string}) => (
+
+   <div className="flex-row-gap-2 items-center">
+       <Image src={icon} alt={alt} width={17} height={17}/>
+       <p>{label}</p>
+   </div>
+)
+
+const EventAgenda=({agendaItems}:{agendaItems:string[]})=>(
+    <div className="agenda">
+        <h2>Agenda</h2>
+        <ul>
+            {agendaItems.map(item=>(
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
+
 const Page =async ({params}:{params: Promise<{slug:string}>}) => {
     const {slug}=await params;
     const request =await fetch(`${BASE_URL}/api/events/${slug}`);
-    const {event:{description,image,overview,date,time,location,mode,agenda,audience}}=await request.json()
+    const {event:{description,image,overview,date,time,location,mode,agenda,organizer,audience}}=await request.json()
 
     if(!description){
         return notFound();
@@ -15,12 +34,31 @@ const Page =async ({params}:{params: Promise<{slug:string}>}) => {
         <section id="event">
             <div className="header">
                 <h1>Event Details:<br/>{slug}</h1>
-                <p className="mt-2">{description}</p>
+                <p>{description}</p>
             </div>
             <div className="details">
 
                 <div className="content">
                     <Image src={image} alt="Event Banner" width={800} height={800} />
+                    <section className="flex-col-gap-2">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+                    <section>
+                        <h2 className="flex-col-gap-2">Event Details</h2>
+                        <EventDetailItem icon="/icons/calendar.svg" alt="calender" label={date}/>
+                        <EventDetailItem icon="/icons/clock.svg" alt="clock" label={time}/>
+                        <EventDetailItem icon="/icons/pin.svg" alt="pin" label={location}/>
+                        <EventDetailItem icon="/icons/mode.svg" alt="mode" label={mode}/>
+                        <EventDetailItem icon="/icons/audience.svg" alt="audience" label={audience}/>
+                    </section>
+                    <EventAgenda agendaItems={JSON.parse(agenda[0])}/>
+
+                    <section className="flex-col-gap-2">
+                        <h2>About the Organizer</h2>
+                        <p>{organizer}</p>
+                    </section>
+
                 </div>
 
                 <aside className="booking">
