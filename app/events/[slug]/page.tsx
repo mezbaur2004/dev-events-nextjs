d4e956392fb162750150c87c9345664c363e69b8
mezbaur2004 <mezbaur2004@gmail.com>
2026-01-04 02:47:01 +0600

    env changed

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -5,7 +5,9 @@ import BookEvent from "@/components/BookEvent";
 import {IEvent} from "@/database";
 import {getSimilarEventsBySlug} from "@/lib/actions/event.actions";
 import EventCard from "@/components/EventCard";
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
+const BASE_URL =
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    (process.env.NODE_ENV === "development" ? "http://127.0.0.1:3000" : "");
 
 const EventDetailItem=({icon,alt,label}:{icon:string,alt:string,label:string}) => (
 
